c8b0698ffa107843fc98405becfc67e3c7dcc94e
VladimirImir <mimimvvv@gmail.com>
2024-02-06 00:27:43 +0200

    Gradle. 27. - Java-library plugin.

--- web/src/main/java/com/dev/servlet/UserServlet.java
+++ web/src/main/java/com/dev/servlet/UserServlet.java
@@ -1,6 +1,7 @@
 package com.dev.servlet;
 
 import com.dev.service.UserService;
+import com.dev.util.StringUtils;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,7 +9,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet("/users")
 public class UserServlet extends HttpServlet {
@@ -22,6 +22,6 @@ public class UserServlet extends HttpServlet {
         var writer = resp.getWriter();
         userService.getAll().forEach(user -> writer.write("""
                 <h1>%d: %s</h1>
-                """.formatted(user.id(), user.name())));
+                """.formatted(user.id(), StringUtils.trim(user.name()))));
     }
 }
